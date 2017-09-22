ce781741dda07f638dc69c9e6d5d6210ff508453
Rayhaan <r53ahmed@uwaterloo.ca>
2017-09-22 15:46:29 -0400

    Update TimingPoint.h

--- TimingPoint.h
+++ TimingPoint.h
@@ -1,8 +1,27 @@
 #pragma once
+
 class TimingPoint
 {
+private:
+	unsigned offset;
+	double msPerBeat;
+	bool inherited;
+	
 public:
-	TimingPoint();
-	~TimingPoint();
-};
+	TimingPoint(unsigned offset, double msPerBeat)
+		: offset(offset), msPerBeat(msPerBeat), inherited(msPerBeat < 0 ? true : false) {}
 
+	~TimingPoint(); 
+
+	unsigned getOffset const () {
+		return offset;
+	}
+
+	double getMsPerBeat const () {
+		return msPerBeat;
+	}
+
+	bool getInherited const () {
+		return inherited;
+	}
+};
