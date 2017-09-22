d4902009cfd4585b4313ed0074cc50dfdb589b61
Rayhaan <r53ahmed@uwaterloo.ca>
2017-09-22 15:46:13 -0400

    Update Beatmap.h

--- Beatmap.h
+++ Beatmap.h
@@ -1,8 +1,35 @@
 #pragma once
+#include "HitObject.h"
+#include "TimingPoint.h"
+#include <vector>
+#include <string>
+#include <memory>
+
 class Beatmap
 {
-public:
-	Beatmap();
-	~Beatmap();
-};
+private:
+	std::string songFilePath;
 
+	// Needed for input timing as period for
+	// a "perfectly" timed hit decreases as 
+	// overall difficulty increases
+	unsigned overallDifficulty;
+	double sliderMultiplier;
+
+	// Wide string for unicode support
+	std::wstring songTitle;
+	std::wstring songArtist;
+
+
+	std::vector<TimingPoint> timingPoints;
+	// Pointer of base type to avoid object slicing
+	std::vector<std::unique_ptr<HitObject>> hitObjects;
+
+public:
+	Beatmap(std::string filePath);
+	~Beatmap();
+
+	bool readSongFile(std::string filePath);
+
+	void printBeatmap const ();
+};
